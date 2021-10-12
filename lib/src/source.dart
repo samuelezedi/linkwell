3d1066b620510bea51b2c3197734f4b3ef0f47e7
Keith Black <tkeithblack@gmail.com>
2021-10-11 19:31:42 -0500

    Updated regex string to better detect urls and email addresses.
    The previous regex string would flag numbers like 60.99 as a url, and it would flag any word with a dot like good.bye or anything with two dots like hello..there as urls.
    
    This new version does not flag these false positives, however, it will only catch emails and urls with the following extensions.
    
    .com
    .ru
    .org
    .gov
    .net
    .ir
    .in
    .uk
    .au
    .de
    .ua
    .edu
    .io
    .co

--- lib/src/source.dart
+++ lib/src/source.dart
@@ -5,7 +5,7 @@ class Helper {
   const Helper(this.value);
 
   static var regex = new RegExp(
-      r"((https?:www\.)|(https?:\/\/)|(www\.))?[\w/\-?=%.][-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?");
+      r"((https?:www\.)|(https?:\/\/)|(www\.))?[\w\-?=%.][-a-zA-Z@:%._\+~#=]{1,256}\.+((com|ru|org|gov|net|ir|in|uk|au|de|ua|edu|io|co))(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?");
 
   static var phoneRegex = new RegExp(r"(^(?:[+0]9)?[0-9]{10,12}$)");
 
